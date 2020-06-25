38174440191da8fb41ecc1111ec13605df177a6b
Dawid Kulma <dawidkulma@gmail.com>
2020-06-25 09:36:54 +0200

    Change random DogRepository filling and Maven settings

--- src/main/java/pl/camp/it/db/DogsRepository.java
+++ src/main/java/pl/camp/it/db/DogsRepository.java
@@ -1,9 +1,11 @@
 package pl.camp.it.db;
 
+import com.github.javafaker.Faker;
 import pl.camp.it.model.Dog;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DogsRepository {
     private final List<Dog> dogs = new ArrayList<>();
@@ -21,5 +23,13 @@ public class DogsRepository {
             dogsRepository = new DogsRepository();
         }
         return dogsRepository;
+    }
+
+    public void fillDogsRepositoryRandomly() {
+        for (int i=0; i<10; i++) {
+            this.dogs.add(new Dog(
+                    new Faker().name().firstName(),
+                    new Random().nextInt(20)));
+        }
     }
 }
